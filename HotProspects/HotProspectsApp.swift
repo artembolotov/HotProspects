5a1aeefaa488b5779c26adc2f7d6f3d40f3f1ae4
Artem Bolotov <teeemon@gmail.com>
2023-03-25 07:33:58 +0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by artembolotov on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
